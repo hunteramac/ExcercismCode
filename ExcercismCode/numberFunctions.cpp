d97289048ab4b92403520fb3234932c1f358dce8
hunteramac <hunter.amac@gmail.com>
2024-07-21 08:56:03 -0700

    Initial commit

---
+++ ExcercismCode/numberFunctions.cpp
@@ -0,0 +1,3 @@
+int getNum() {
+	return 5;
+}
---
+++ ExcercismCode_Tests/test.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+
+//#include "numberFunctions.cpp"
+#include "../ExcercismCode/numberFunctions.cpp"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(getNum(), 5);
+}
