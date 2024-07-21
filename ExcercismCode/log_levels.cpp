79c13aca4bbc4137ede164171ebc1e1de186d70c
hunteramac <hunter.amac@gmail.com>
2024-07-21 09:07:20 -0700

    add excercism log levels

---
+++ ExcercismCode/log_levels.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+namespace log_line {
+    std::string message(std::string line) {
+        // return the message
+        return "";
+    }
+
+    std::string log_level(std::string line) {
+        // return the log level
+        return "";
+    }
+
+    std::string reformat(std::string line) {
+        // return the reformatted message
+        return "";
+    }
+}
