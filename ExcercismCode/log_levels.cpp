48633e4102863a5cfd18e1d955a4a92b37ac530a
Hunter MacNab <hunter.amac@gmail.com>
2024-07-21 10:04:59 -0700

    add code pass test for log_level

--- ExcercismCode/log_levels.cpp
+++ ExcercismCode/log_levels.cpp
@@ -3,16 +3,20 @@
 #include <string>
 
 namespace log_line {
+    const std::string LOG_LEVEL_LINE_DELIMITER_START = "[";
+    const std::string LOG_LEVEL_LINE_DELIMITER_END = "]: ";
+
     std::string message(std::string line) {
         // return the message
-        const std::string LINE_DELIMITER = "]: ";
-        int Index_MessageStart = line.find(LINE_DELIMITER)+LINE_DELIMITER.size();
+        int Index_MessageStart = line.find(LOG_LEVEL_LINE_DELIMITER_END)+LOG_LEVEL_LINE_DELIMITER_END.size();
         return line.substr(Index_MessageStart, std::string::npos);
     }
 
     std::string log_level(std::string line) {
         // return the log level
-        return "";
+        int Index_LogLevelStart = line.find(LOG_LEVEL_LINE_DELIMITER_START)+1;
+        int Index_LogLevelEnd = line.find(LOG_LEVEL_LINE_DELIMITER_END)-1;
+        return line.substr(Index_LogLevelStart,Index_LogLevelEnd);
     }
 
     std::string reformat(std::string line) {
