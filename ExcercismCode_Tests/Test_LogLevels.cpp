f27dfb6b8381896b5a76fd47c7868fdf78462aa6
hunteramac <hunter.amac@gmail.com>
2024-07-21 09:31:48 -0700

    add failing test

--- ExcercismCode_Tests/Test_LogLevels.cpp
+++ ExcercismCode_Tests/Test_LogLevels.cpp
@@ -10,3 +10,8 @@ TEST(Ex_LogLevels, T1) {
 	std::string line = "[INFO]: hello world";
 	EXPECT_EQ(log_line::message(line),"hello world");
 }
+
+TEST(Ex_LogLevels, T2) {
+	std::string line = "[INFO]: good bye world";
+	EXPECT_EQ(log_line::message(line), "good bye world");
+}
