9e9f1f90df2136c1780c06ba3f47d97b6c3591e3
Hunter MacNab <hunter.amac@gmail.com>
2024-07-21 10:13:56 -0700

    add failing test

--- ExcercismCode/log_levels.cpp
+++ ExcercismCode/log_levels.cpp
@@ -3,15 +3,31 @@
 #include <string>
 
 namespace log_line {
+    // A log line is of format
+    //[<LEVEL>]: <MESSAGE>
+    // Where <LEVEL> can be one of three values
+    //INFO
+    //WARNING
+    //ERROR
     const std::string LOG_LEVEL_LINE_DELIMITER_START = "[";
     const std::string LOG_LEVEL_LINE_DELIMITER_END = "]: ";
 
+    /// <summary>
+    /// Returns the message of a log line
+    /// </summary>
+    /// <param name="line"></param>
+    /// <returns></returns>
     std::string message(std::string line) {
         // return the message
         int Index_MessageStart = line.find(LOG_LEVEL_LINE_DELIMITER_END)+LOG_LEVEL_LINE_DELIMITER_END.size();
         return line.substr(Index_MessageStart, std::string::npos);
     }
 
+    /// <summary>
+    /// Returns a log line's log level in upper case
+    /// </summary>
+    /// <param name="line"></param>
+    /// <returns></returns>
     std::string log_level(std::string line) {
         // return the log level
         int Index_LogLevelStart = line.find(LOG_LEVEL_LINE_DELIMITER_START)+1;
@@ -19,6 +35,11 @@ namespace log_line {
         return line.substr(Index_LogLevelStart,Index_LogLevelEnd);
     }
 
+    /// <summary>
+    /// Reformats the log line, putting the message first and the log level after it in parentheses
+    /// </summary>
+    /// <param name="line">Log line to proccess</param> 
+    /// <returns>A string containing logs message, then the log level in parentheses</returns> 
     std::string reformat(std::string line) {
         // return the reformatted message
         return "";
--- ExcercismCode_Tests/Test_LogLevels.cpp
+++ ExcercismCode_Tests/Test_LogLevels.cpp
@@ -20,3 +20,8 @@ TEST(Ex_LogLevels, log_level_1) {
 	std::string line = "[INFO]: good bye world";
 	EXPECT_EQ(log_line::log_level(line), "INFO");
 }
+
+TEST(Ex_LogLevels, reformat_1) {
+	std::string line = "[INFO]: good bye world";
+	EXPECT_EQ(log_line::reformat(line), "good bye world (INFO)");
+}
