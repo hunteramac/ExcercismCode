5dc35a7999f5e971de525c83bf90bc4e5877aaeb
Hunter MacNab <hunter.amac@gmail.com>
2024-08-07 07:07:49 -0700

    fix passing for vehicle purchase

--- ExcercismCode/vehicle_purchase.cpp
+++ ExcercismCode/vehicle_purchase.cpp
@@ -1,35 +1,33 @@
-#include <string>
+#include "vehicle_purchase.h"
 
-bool needs_license(std::string vehicle) {
-	if (vehicle == "car" ||
-		vehicle == "truck")
+using namespace vehicle_purchase;
+
+bool vehicle_purchase::needs_license(std::string kind) {
+	if (kind == "car" ||
+		kind == "truck")
 		return true;
 	else
 		return false;
 }
 
-std::string choose_vehicle(std::string VehicleOptionA, std::string VehicleOptionB) {
+std::string vehicle_purchase::choose_vehicle(std::string option1, std::string option2) {
 	std::string append = " is clearly the better choice.";
 
-	//Which is first in lexigraphical order?
-
-	//can we just compare? yes we can, operator must be overloaded
-	if (VehicleOptionA <= VehicleOptionB)
-		return VehicleOptionA + append;
+	if (option1 <= option2)
+		return option1 + append;
 	else
-		return VehicleOptionB + append;
+		return option2 + append;
 }
 
-//Something buggy here
-double calculate_resell_price(double originalVehiclePrice, int vehicleAge) {
-	if (vehicleAge == 0)
-		return originalVehiclePrice;
-	else if (vehicleAge > 0 && vehicleAge < 3)
-		return originalVehiclePrice * .8;
-	else if (vehicleAge >= 3 && vehicleAge < 10)
-		return originalVehiclePrice * .7;
-	else if(vehicleAge>=10)
-		return originalVehiclePrice*.5;
+double vehicle_purchase::calculate_resell_price(double original_price, double age) {
+	if (age == 0)
+		return original_price;
+	else if (age > 0 && age < 3)
+		return original_price * .8;
+	else if (age >= 3 && age < 10)
+		return original_price * .7;
+	else if(age>=10)
+		return original_price*.5;
 
-	return 0;
+	return original_price;
 }
