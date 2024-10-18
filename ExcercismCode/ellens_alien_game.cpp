1b3aceaf10f68e243b89a460fedc2e23c48628fa
hunteramac <hunter.amac@gmail.com>
2024-10-17 21:22:12 -0700

    ellens alien game: implement coords, health, hit, is alive

--- ExcercismCode/ellens_alien_game.cpp
+++ ExcercismCode/ellens_alien_game.cpp
@@ -1,11 +1,29 @@
+namespace targets{
 class Alien {
-	/*
-	int x_coordinate;
-	int y_coordinate*/
+private:
+	int health;
+
 public:
+	int x_coordinate;
+	int y_coordinate;
+
 	Alien(int x, int y);
+
+	int get_health() { return health; }
+
+	void hit();
+
+	bool is_alive() { return (health > 0); };
 };
 
 Alien::Alien(int x, int y) {
-
+	x_coordinate = x;
+	y_coordinate = y;
+	health = 3;
 }
+
+void Alien::hit() {
+	--health;
+}
+
+} // namespace targets
--- ExcercismCode_Tests/TestEllensAlienGame.cpp
+++ ExcercismCode_Tests/TestEllensAlienGame.cpp
@@ -1,14 +1,68 @@
 #include "pch.h"
 
 #include "../ExcercismCode/ellens_alien_game.cpp"
+using namespace targets;
 
 namespace EllensAlienGame {
-	TEST(Dummy, testHarnessWorks) {
-		EXPECT_TRUE(true);
-	}
+TEST(Dummy, testHarnessWorks) {
+	EXPECT_TRUE(true);
+}
 
-	TEST(Constructor, canBeCalled) {
-		// It can construct
-		Alien testAlien(5, 2);
-	}
+TEST(Constructor, canBeCalled) {
+	// It can construct
+	Alien testAlien(5, 2);
+}
+
+TEST(Coordinates, canBeAccessed) {
+	Alien testAlien(5, 2);
+
+	EXPECT_EQ(5, testAlien.x_coordinate);
+	EXPECT_EQ(2, testAlien.y_coordinate);
+}
+
+TEST(Health, canBeAccessed) {
+	Alien testAlien(5, 2);
+
+	// ACT
+	int result = testAlien.get_health();
+
+	EXPECT_EQ(result, 3);
+}
+
+//fixture to construct an alien?
+
+TEST(Hit, DecrementsHealthByOne) {
+	// ARRANGE
+	Alien testAlien(5, 2);
+	int prevHealth = testAlien.get_health();
+
+	//ACT
+	testAlien.hit();
+	int newHealth = testAlien.get_health();
+
+	// ASSERT
+	EXPECT_EQ(newHealth, prevHealth - 1);
+}
+
+TEST(IsAlive, onAlienConstruct)
+{
+	Alien testAlien(5, 2);
+	EXPECT_TRUE(testAlien.is_alive());
+}
+
+TEST(IsAlive, onAlienHit3x)
+{
+	Alien testAlien(5, 2);
+
+	// defn efficent way to itterate some number times
+	//post increment essential here, otherwise only executes twice
+	for (auto _ = 3; _--;) testAlien.hit();
+	//for (int i = 0; i < 3; ++i) {
+	//	testAlien.hit();
+	//}
+
+	EXPECT_EQ(testAlien.get_health(), 0);
+	EXPECT_FALSE(testAlien.is_alive());
+}
+
 }
