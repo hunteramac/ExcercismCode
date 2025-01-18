0c074a6ba3813487a4e29f889ce603b9b4d5e907
hunteramac <hunter.amac@gmail.com>
2025-01-18 09:06:49 -0800

    fin elen's alien game

--- ExcercismCode/ellens_alien_game.cpp
+++ ExcercismCode/ellens_alien_game.cpp
@@ -1,19 +1,23 @@
 namespace targets{
 class Alien {
-private:
-	int health;
+	private:
+		int health;
 
-public:
-	int x_coordinate;
-	int y_coordinate;
+	public:
+		int x_coordinate;
+		int y_coordinate;
 
-	Alien(int x, int y);
+		Alien(int x, int y);
 
-	int get_health() { return health; }
+		int get_health() { return health; }
 
-	void hit();
+		bool hit();
 
-	bool is_alive() { return (health > 0); };
+		bool is_alive() { return (health > 0); };
+
+		bool teleport(int x, int y);
+
+		bool collision_detection(Alien check);
 };
 
 Alien::Alien(int x, int y) {
@@ -22,8 +26,27 @@ Alien::Alien(int x, int y) {
 	health = 3;
 }
 
-void Alien::hit() {
-	--health;
+bool Alien::collision_detection(Alien check) {
+	if (
+		this->x_coordinate == check.x_coordinate &&
+		this->y_coordinate == check.y_coordinate
+		)
+		return true;
+	else
+		return false;
+}
+
+bool Alien::hit() {
+	if(health>0)
+		--health;
+
+	return true;
+}
+
+bool Alien::teleport(int x, int y) {
+	x_coordinate = x;
+	y_coordinate = y;
+	return true;
 }
 
 } // namespace targets
--- ExcercismCode_Tests/TestEllensAlienGame.cpp
+++ ExcercismCode_Tests/TestEllensAlienGame.cpp
@@ -44,6 +44,19 @@ TEST(Hit, DecrementsHealthByOne) {
 	EXPECT_EQ(newHealth, prevHealth - 1);
 }
 
+TEST(Hit, SuccessfulHit) {
+	Alien alien(1, 1);
+	EXPECT_TRUE(alien.hit());
+}
+
+TEST(Hit, HealthDoesNotDropBellowOne) {
+	Alien testAlien(1, 1);
+
+	for (auto _ = 4; _--;) testAlien.hit();
+
+	EXPECT_EQ(testAlien.get_health(), 0);
+}
+
 TEST(IsAlive, onAlienConstruct)
 {
 	Alien testAlien(5, 2);
@@ -63,6 +76,28 @@ TEST(IsAlive, onAlienHit3x)
 
 	EXPECT_EQ(testAlien.get_health(), 0);
 	EXPECT_FALSE(testAlien.is_alive());
+}
+
+TEST(AlienTeleport, newCoords1) {
+	Alien testAlien(0, 0);
+
+	EXPECT_TRUE(testAlien.teleport(5, 4));
+	EXPECT_EQ(5, testAlien.x_coordinate);
+	EXPECT_EQ(4, testAlien.y_coordinate);
+}
+
+TEST(CollisionDetection, SameCoords) {
+	Alien alien1(0, 0);
+	Alien alien2(0, 0);
+
+	EXPECT_TRUE(alien1.collision_detection(alien2));
+}
+
+TEST(CollisionDetection, DiffCoords) {
+	Alien alien1(0, 0);
+	Alien alien2(1, 1);
+
+	EXPECT_FALSE(alien1.collision_detection(alien2));
 }
 
 }
