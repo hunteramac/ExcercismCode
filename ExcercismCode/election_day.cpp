872e701b3d093f2e0ee92bfe3cc73235230d6a66
hunteramac <hunter.amac@gmail.com>
2025-01-21 06:46:07 -0800

    election_day: t2

--- ExcercismCode/election_day.cpp
+++ ExcercismCode/election_day.cpp
@@ -12,7 +12,6 @@ namespace election {
         int votes{};
     };
 
-    // TODO: Task 1
     // vote_count takes a reference to an `ElectionResult` as an argument and will
     // return the number of votes in the `ElectionResult.
     int vote_count(ElectionResult& election) {
@@ -23,7 +22,9 @@ namespace election {
     // increment_vote_count takes a reference to an `ElectionResult` as an argument
     // and a number of votes (int), and will increment the `ElectionResult` by that
     // number of votes.
-
+    void increment_vote_count(ElectionResult& election, int increment) {
+        election.votes += increment;
+    }
 
     // TODO: Task 3
     // determine_result receives the reference to a final_count and returns a
--- ExcercismCode_Tests/Test_ElectionDay.cpp
+++ ExcercismCode_Tests/Test_ElectionDay.cpp
@@ -13,4 +13,16 @@ namespace ElectionDay {
 		ElectionResult hamilton{ "Alex", 1804 };
 		EXPECT_EQ(vote_count(hamilton), 1804);
 	}
+
+	TEST(IncrementVoteCount, IncrementByOne) {
+		ElectionResult hamilton{ "Alex", 1804 };
+
+		increment_vote_count(hamilton, 1);
+
+		EXPECT_EQ(1805, vote_count(hamilton));
+
+		increment_vote_count(hamilton, 3);
+
+		EXPECT_EQ(1808, vote_count(hamilton));
+	}
 };
