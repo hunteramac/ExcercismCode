1b816c784a2f9570e8a7149589e7d5537abce6a7
hunteramac <hunter.amac@gmail.com>
2025-01-22 18:35:45 -0800

    troll_the_trolls: add handling task 2-3

--- ExcercismCode/troll_the_trolls.cpp
+++ ExcercismCode/troll_the_trolls.cpp
@@ -20,16 +20,30 @@ namespace hellmath {
 	// of `AccountStatus` and returns a `bool`. The first argument is the status of
 	// the poster, the second one is the status of the viewer.
 	bool display_post(AccountStatus poster, AccountStatus viewer) {
-		return false;
+		if (poster == AccountStatus::troll && viewer != AccountStatus::troll)
+			return false;
+		else
+			return true;
 	}
 
 	// TODO: Task 3 - Implement the `permission_check` function, that takes an
 	// `Action` as a first argument and an `AccountStatus` to check against. It
 	// should return a `bool`.
+	bool permission_check(Action check, AccountStatus user) {
+		if (check == Action::write && user == AccountStatus::guest)
+			return false;
+		else if (check == Action::remove && user != AccountStatus::mod)
+			return false;
+		else
+			return true;
+	}
 
 	// TODO: Task 4 - Implement the `valid_player_combination` function that
 	// checks if two players can join the same game. The function has two parameters
 	// of type `AccountStatus` and returns a `bool`.
+	bool valid_player_combination(AccountStatus player1, AccountStatus player2) {
+		return true;
+	}
 
 	// TODO: Task 5 - Implement the `has_priority` function that takes two
 	// `AccountStatus` arguments and returns `true`, if and only if the first
--- ExcercismCode_Tests/Test_TrollTheTrolls.cpp
+++ ExcercismCode_Tests/Test_TrollTheTrolls.cpp
@@ -4,12 +4,77 @@
 using namespace hellmath;
 
 namespace TrollTheTrolls {
+
+	std::vector<AccountStatus> allUserTypes = {
+		AccountStatus::troll,
+		AccountStatus::guest,
+		AccountStatus::user,
+		AccountStatus::mod
+	};
 	TEST(Dummy, AlwaysTrue) {
 		EXPECT_TRUE(true);
 	}
 
 	TEST(DisplayPost, DontDisplayTrollPostsToUsers) {
-		bool result = display_post(AccountStatus::troll, AccountStatus::user);
-		EXPECT_FALSE(result);
+		EXPECT_FALSE(display_post(AccountStatus::troll, AccountStatus::user));
+	}
+
+	TEST(DisplayPost, DisplayNonTrollPostsToAll) {
+		// How can we paramterize this test?
+		std::vector<AccountStatus> noTrolls = {
+			AccountStatus::guest,
+			AccountStatus::user,
+			AccountStatus::mod
+		};
+
+		for( auto curTypeNoTrolls : noTrolls)
+			for (auto curType: allUserTypes)
+				EXPECT_TRUE(display_post(curTypeNoTrolls, curType));
+	}
+
+	TEST(DisplayPost, OnlyTrollsSeeTrollPosts) {
+		// in C++ there appears to not currently be a good way to itterate over an Enum'S possible values
+		// therefore, we must make do in these tests with arrays that will become out of date if new
+		// entries to the enum are added.
+		std::vector<AccountStatus> Types = {
+			AccountStatus::guest,
+			AccountStatus::user,
+			AccountStatus::mod
+		};
+
+		EXPECT_TRUE(display_post(AccountStatus::troll, AccountStatus::troll));
+
+		for (auto curType : Types) {
+			EXPECT_FALSE(display_post(AccountStatus::troll, curType));
+		}
+	}
+
+	TEST(PermissionCheck, call) {
+		permission_check(Action::remove, AccountStatus::troll);
+	}
+
+	TEST(PermissionCheck, AllUsersCanReadPosts) {
+		for(auto user : allUserTypes)
+			EXPECT_TRUE(permission_check(Action::read, user));
+	}
+
+	TEST(PermissionCheck, AllUsersButGuestsCanWrite) {
+		for (auto user : allUserTypes)
+			if (user == AccountStatus::guest)
+				EXPECT_FALSE(permission_check(Action::write, user));
+			else
+				EXPECT_TRUE(permission_check(Action::write, user));
+	}
+
+	TEST(PermissionCheck, OnlyModeratorsCanRemovePosts) {
+		for (auto user : allUserTypes) {
+			if (user == AccountStatus::mod)
+				EXPECT_TRUE(permission_check(Action::remove, user));
+			else
+				EXPECT_FALSE(permission_check(Action::remove, user));
+		}
+	}
+	TEST(ValidPlayerCombination, call) {
+		valid_player_combination(AccountStatus::troll, AccountStatus::guest);
 	}
 };
